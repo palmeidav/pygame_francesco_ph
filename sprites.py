cd4d85685b8e54778da0b3d22357c809bc234ce3
Francesco Rodrigues <keko.vaccario@gmail.com>
2023-01-30 11:15:15 -0300

    tamanho da caixa

--- sprites.py
+++ sprites.py
@@ -7,11 +7,12 @@ class Numero(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = dicionario_de_arquivos['caixa'] # assets é um dicionário de imagens, sons e fonts
+        self.image = pygame.transform.scale(self.image, (70, 70))
         #todo objeto precisa de um rect
         # rect é a representação de retangulo feita pelo pygame
         self.rect = self.image.get_rect()
-        self.rect.x =  random.randint(0,900)# defina posicao em x
-        self.rect.y = random.randint(0,700) # defina posicao em y
+        self.rect.x =  random.randint(0,900-70)# defina posicao em x
+        self.rect.y = random.randint(0,700-70) # defina posicao em y
 
 class Botao(pygame.sprite.Sprite):
     def __init__(self, assets, nome_do_jogo):
